4d9697cc5c4ed490c7d4c4a5a192029cbac92953
Vamsavardan Vemuru <vamsi0493@gmail.com>
2015-11-24 11:04:35 +0530

    login, signup

--- js/login.js
+++ js/login.js
@@ -1,7 +1,10 @@
-var login_cd = function(showhide){
+showhide = "show"
+var login_cd = function(){
 if(showhide == "show"){
     document.getElementById('popupbox').style.visibility="visible";
+    showhide = "hide"
 }else if(showhide == "hide"){
     document.getElementById('popupbox').style.visibility="hidden"; 
+    showhide = "show"
 }
 }
